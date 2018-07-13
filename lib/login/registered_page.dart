4ba616c97fca8b41dba4fc853b4925ad4ad8e544
liximing1 <hun342508075@126.com>
2018-07-13 17:26:20 +0800

    更新 登录注册页面，添加链接数据库验证逻辑

--- lib/login/registered_page.dart
+++ lib/login/registered_page.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_database/firebase_database.dart';
 class RegisteredPage extends StatefulWidget{
   VoidCallback voidCallback;
-  RegisteredPage(this.voidCallback);
+
+
+  RegisteredPage(this.voidCallback,);
   @override
   State<StatefulWidget> createState() =>RegisteredPageState();
 
 }
 
 class RegisteredPageState extends State<RegisteredPage>{
-  TextEditingController _controller;
+  TextEditingController _nameController;
+  TextEditingController _passwordController;
+  TextEditingController _passwordController2;
+  TextEditingController _nickNameController;
+  DatabaseReference reference;
+  bool _usernameErr = false;
+  bool _passwordErr = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _nameController = new TextEditingController();
+    _passwordController = new TextEditingController();
+    _passwordController2 = new TextEditingController();
+    _nickNameController = new TextEditingController();
+    reference = FirebaseDatabase.instance.reference().child('users');
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Center(
@@ -20,8 +41,22 @@ class RegisteredPageState extends State<RegisteredPage>{
               new Container(
                 width: MediaQuery.of(context).size.width/2,
                 child:new TextField(
-                  controller: _controller,
-                  decoration: const InputDecoration(
+                  onSubmitted: (value){
+                    reference.child(value).once().then((DataSnapshot onValue){
+                      if(onValue.value==null){
+                        _usernameErr = true;
+                        setState(() {
+                        });
+                      }else{
+                        _usernameErr = false;
+                        setState(() {
+                        });
+                      }
+                    });
+                  },
+                  controller: _nameController,
+                  decoration:  InputDecoration(
+                    errorText: _usernameErr ? null:'用户名被占用',
                     fillColor: Colors.white,
                     labelText:'用户名：',
                     labelStyle:TextStyle(fontSize: 15.0,color: Colors.white70),
@@ -31,7 +66,7 @@ class RegisteredPageState extends State<RegisteredPage>{
               new Container(
                 width: MediaQuery.of(context).size.width/2,
                 child: new TextField(
-                  controller: _controller,
+                  controller: _passwordController,
                   decoration: InputDecoration(
                       labelText: '密码：',
                       labelStyle: TextStyle(fontSize: 15.0,color: Colors.white70)
@@ -41,9 +76,27 @@ class RegisteredPageState extends State<RegisteredPage>{
               new Container(
                 width: MediaQuery.of(context).size.width/2,
                 child: new TextField(
-                  controller: _controller,
+                  onSubmitted: (value){
+                    if(value==_passwordController.text){
+                      _passwordErr = true;
+                    }else{
+                      _passwordErr = false;
+                    }
+                  },
+                  controller:_passwordController2,
                   decoration: InputDecoration(
-                      labelText: '确认密码：',
+                    errorText: _passwordErr ? null: '两次密码不同┗|｀O′|┛ 嗷~~',
+                    labelText: '确认密码：',
+                    labelStyle: TextStyle(fontSize: 15.0,color: Colors.white70)
+                  ),
+                ),
+              ) ,
+              new Container(
+                width: MediaQuery.of(context).size.width/2,
+                child: new TextField(
+                  controller:_nickNameController,
+                  decoration: InputDecoration(
+                      labelText: '昵称：',
                       labelStyle: TextStyle(fontSize: 15.0,color: Colors.white70)
                   ),
                 ),
@@ -56,7 +109,19 @@ class RegisteredPageState extends State<RegisteredPage>{
                     borderRadius: BorderRadius.all(Radius.circular(5.0))
                 ),
                 child: new FlatButton(
-                    onPressed: null,
+                    onPressed: (_usernameErr&&_passwordErr)?(){
+                      setState(() {
+                        reference.child(_nameController.text).set({
+                          'username':_nameController.text,
+                          'password':_passwordController.text,
+                          'nickName':_nickNameController.text
+                        });
+                        _nameController.clear();
+                        _passwordController.clear();
+                        _passwordController2.clear();
+                        _nickNameController.clear();
+                      });
+                    }:null,
                     child: new Text('注册',style: new TextStyle(
                         color: Colors.black
                     ),)),
